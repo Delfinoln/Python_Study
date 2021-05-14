4c6763a9a44c4f5be6546dbc83e6f8e6655e3f46
Delfinoln <delfino.ln@hotmail.com>
2021-05-14 09:00:08 -0300

    Created and set up screen

--- 100DaysOfCode-TheCompletePythonProBootcampFor2021-Course/Day22-Project/Pong-Game/main.py
+++ 100DaysOfCode-TheCompletePythonProBootcampFor2021-Course/Day22-Project/Pong-Game/main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import turtle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Create a screen objet from turtle module
+screen = turtle.Screen()
+
+# Setting up screen elements
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Only close the screen when click a button
+screen.exitonclick()
