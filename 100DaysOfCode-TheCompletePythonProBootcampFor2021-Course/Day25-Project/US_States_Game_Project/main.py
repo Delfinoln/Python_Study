e8465e15c99f262ec11a9af71d44e05891c8cde2
Delfinoln <delfino.ln@hotmail.com>
2021-05-17 17:48:23 -0300

    Bug correction

--- 100DaysOfCode-TheCompletePythonProBootcampFor2021-Course/Day25-Project/US_States_Game_Project/main.py
+++ 100DaysOfCode-TheCompletePythonProBootcampFor2021-Course/Day25-Project/US_States_Game_Project/main.py
@@ -27,26 +27,26 @@ writer.hideturtle()
 
 game_is_on = True
 STATES = 50
-correct_states = 0
+correct_states = []
 
-while game_is_on and correct_states < 50:
+while game_is_on and len(correct_states) < 50:
     # Asking user
-    answer_state = screen.textinput(title=f"{correct_states}/{STATES} States Correct", prompt="What's another state's name?").title()
+    answer_state = screen.textinput(title=f"{len(correct_states)}/{STATES} States Correct", prompt="What's another state's name?").title()
 
     # Check if user's answer exists in the data frame
     if answer_state in data_frame["state"].to_list():
         exists = True
 
     # Write on screen the name of the state
-    if exists:
-        correct_states += 1
+    if exists and answer_state not in correct_states:
+        correct_states.append(answer_state)
         state_x = data_frame[data_frame["state"] == answer_state]["x"]
         state_y = data_frame[data_frame["state"] == answer_state]["y"]
         writer.goto(int(state_x), int(state_y))
         writer.write(answer_state, align="center", font=("Courier", 8, "normal"))
         exists = False
 
-
+print("You got all 50 states!")
 
 
 screen.exitonclick()
