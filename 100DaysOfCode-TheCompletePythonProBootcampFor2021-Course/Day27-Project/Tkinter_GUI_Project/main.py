87d5353dd6fae0340d6afd330063c062739857c6
Delfinoln <delfino.ln@hotmail.com>
2021-05-21 22:02:27 -0300

    Update main.py

--- 100DaysOfCode-TheCompletePythonProBootcampFor2021-Course/Day27-Project/Tkinter_GUI_Project/main.py
+++ 100DaysOfCode-TheCompletePythonProBootcampFor2021-Course/Day27-Project/Tkinter_GUI_Project/main.py
@@ -2,11 +2,37 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("My first GUI program")
-window.minsize(width= 500, height=300)
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
-#Label
+# Label
 
 my_label = tkinter.Label(text='I am a Label', font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.grid(column=0, row=0)
+
+my_label["text"] = "New Text"
+# OR
+my_label.config(text="New Text")
+
+
+# Button
+
+def button_clicked():
+    my_label["text"] = input.get()
+
+
+button = tkinter.Button(text='Click Me', command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = tkinter.Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+
+# Entry
+
+input = tkinter.Entry(width=10)
+input.grid(column=3, row=2)
+
+
 
 window.mainloop()
